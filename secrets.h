dfc3c1f3399c2d9941d6ce72c13cb56cfda3149c
Saad Imtiaz <53612215+Saad-Imtiaz@users.noreply.github.com>
2023-12-14 23:10:42 +0500

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "SINGTEL-7B11"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "ipireingez"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "xxx"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "xxx"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 1291602			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "BR0FSB0E4DK6WYRE"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID xxx			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "xxx"   // replace XYZ with your channel write API Key
